e9fcacc6323e14e3794ef55a1dbf3adda1bbf8e9
Alexander <alexanderblanchard@yahoo.com>
2023-04-18 12:22:46 +0100

    removing unneeded borders

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,7 +5,7 @@ import './aboutMe.css';
 const AboutMe = () => {
   return (
     // <div name="aboutMeContainer" className="w-full h-600 bg-gradient-to-b from-gray-800 to-black text-white">
-    <div  id="me" className='w-full h-fit-content bg-gradient-to-b from-gray-800 to-black text-white'>
+    <div  id="me" name="aboutMeContainer" className='w-full h-fit-content bg-gradient-to-b from-gray-800 to-black text-white'>
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         {/* <div className="pb-8"> */}
           {/* <p className="text-3xl font-bold inline border-b-4 border-gray-500">
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,12 +3,13 @@ import { FaGithub, FaFileDownload } from "react-icons/fa";
 import { BsFillTelephoneFill } from "react-icons/bs";
 import { HiOutlineMail } from "react-icons/hi";
 import AlexanderBlanchardCV from "../assets/AlexanderBlanchardCV.pdf";
+import "./contactMe.css";
 
 const Contact = () => {
 
    
   return (
-    <div name="contactContainer" className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
+    <div id="contact" name="contactContainer" className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
          <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
             <div className="pb-8">
                 <p className="text-2xl inline text-pink-400 border-b-4 border-blue-400">
